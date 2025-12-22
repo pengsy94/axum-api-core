1944bac894567790497a8ba26498df0ae7e8b32c
pengsy <songyang410@outlook.com>
2025-12-22 21:48:45 +0800

    initialize

--- api/src/route.rs
+++ api/src/route.rs
@@ -3,7 +3,7 @@ use crate::routes;
 use axum::Router;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse};
-use axum::routing::{get, get_service};
+use axum::routing::{get, get_service, post};
 use common::config::{RESOURCE_DIR, WEB_STATIC_DIR, server_config};
 use core::engine;
 use orm::repository::sys_user;
@@ -29,7 +29,24 @@ fn add_web_routes(mut router: Router) -> Router {
 
     if config.debug {
         //  测试模块
-        router = router.nest("/test", Router::new().route("/ws", get(websocket)));
+        router = router.nest(
+            "/test",
+            Router::new()
+                // websocket 页面
+                .route("/ws", get(websocket))
+                // 获取参数 /{id}
+                .route("/{id}", get(routes::case::args::sys_path_test))
+                .route("/{name}/{age}", get(routes::case::args::sys_path_2_test))
+                .route("/query", get(routes::case::args::sys_query_test))
+                // header获取
+                .route("/header", get(routes::case::args::sys_header_test))
+                // 返回json
+                .route("/json", get(routes::case::args::sys_response_json))
+                // post json提交参数
+                .route("/post-json", post(routes::case::args::sys_query_json))
+                // post form提交参数
+                .route("/post-form", post(routes::case::args::sys_query_form)),
+        );
     }
 
     router = router.route("/", get(index)).nest(
--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -1,4 +1,5 @@
 pub mod websocket;
+pub mod case;
 
 
 
