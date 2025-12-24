368ed23f1e6f0aad0223bdf9ea719cea9986173d
pengsy <songyang410@outlook.com>
2025-12-24 22:10:55 +0800

    style: format code with prettier

--- kernel/src/tasks/task_one.rs
+++ kernel/src/tasks/task_one.rs
@@ -1,10 +1,10 @@
 use tokio_cron_scheduler::{Job, JobSchedulerError, job::JobLocked};
-use tracing::error;
+use tracing::{error, info};
 
 /// 实现示例任务
 pub fn handle_task() -> Result<JobLocked, JobSchedulerError> {
     let jon = Job::new("*/10 * * * * *", |_uuid, _l| {
-        println!(
+        info!(
             "示例任务执行 - 时间: {}",
             chrono::Local::now().format("%Y-%m-%d %H:%M:%S")
         );
