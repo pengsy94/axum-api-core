c80cd39b280123f7297c732cfb5f515da88f5c4a
pengsy <songyang410@outlook.com>
2026-01-09 13:19:04 +0800

    chore: 杂项处理

--- app/src/api/system.rs
+++ app/src/api/system.rs
@@ -8,7 +8,10 @@ pub async fn login(
 ) -> ApiResponse<LoginResponse> {
     println!("{:?}", payload);
 
-    ApiResponse::success(LoginResponse {
-        token: String::from("晓风残月"),
-    })
+    let response = LoginResponse {
+        token: String::from("冢中枯骨，吾早晚必擒之！"),
+        token_type: String::from("追比圣贤，本是读书人的愿望！"),
+        message: String::from("为天地立心，为生民立命，为往圣继绝学，为万世开太平!"),
+    };
+    ApiResponse::success(response)
 }
--- common/src/response/login.rs
+++ common/src/response/login.rs
@@ -3,4 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Debug, Serialize)]
 pub struct LoginResponse {
     pub token: String,
+    pub token_type: String,
+    pub message: String,
 }
